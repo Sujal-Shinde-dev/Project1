a5097aafad54cb8286632ce81f8a4aab802c5897
Sujal-Shinde <sujalshinde1404@gmail.com>
2025-01-13 13:59:16 +0530

    Project Done

---
+++ Project/jobs/job-list.js
@@ -0,0 +1,32 @@
+const jCategories=[
+    {
+        index:0,
+        title:'Web Developer',
+        image:'../images/google.png',
+        av:'Full Time',
+        rate:"$900-1200/m",
+        companyName:"Google",
+        location:"USA",
+        vacany:"01",
+        hours:"50h / Week",
+        description:"A Google Developer refers to an individual or a professional engaged in creating applications, solutions, or services using Google's tools, technologies, and platforms. Google provides a vast ecosystem for developers to innovate, ranging from cloud computing to mobile and web development tools.",
+        workplace:"Work From Home",
+        education:"Bachlor degree in any computer Science Course",
+        experience:"2 to 5 years(s)"
+
+    },
+    {
+        index:1,
+        title:'Buisness Associate',
+        image:'../images/uber.png',
+        av:'Part Time',
+        rate:"$900-1200/m",
+        companyName:"Uber",
+        location:"USA",
+        vacany:"04",
+        hours:"40h / Week",
+        description:"A Ride When You Need It — Get A Ride With A Few Taps. Watch In The App As Your Driver Arrives To Your Location. Upfront Pricing On Every Trip So You Have An Idea Of What You'll Pay. Ride with Care.",
+        education:"Bachlor degree in any computer Science Course",
+        experience:"1 to 5 years(s)"
+    },
+]
